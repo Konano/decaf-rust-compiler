065c35a31d87b097a91d3b1b8bb0278980542123
Konano <nanoapezlk@gmail.com>
2019-11-29 00:06:07 +0800

    to PA3

--- driver/src/test.rs
+++ driver/src/test.rs
@@ -1,7 +1,15 @@
 use driver::*;
 
 fn main() {
-  for result in test_all("testcase/S2-rs", Pa::Pa2).unwrap() {
+  for result in test_all("testcase/S3", Pa::Pa3).unwrap() {
     println!("{:?}", result);
   }
+  //   eprintln!("{:?}", compile(r#"
+  // class Main {
+  //  static void f(class Main m) {}
+  //  static void main() {
+  //    f(Main)  ;
+  //  }
+  // }
+  //   "#, &Alloc::default(), Pa::Pa2.to_cfg() ));
 }
